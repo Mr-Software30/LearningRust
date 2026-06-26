00da1cc818c367cc56cb3ab18be4ddd71217a501
Mr-Software30 <warzermuhammed5@gmail.com>
2026-06-26 23:12:25 +0300

    secret number was not a secret, not it is

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,7 +5,7 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number between 1 and 100!");
     let secrete_number = rand::thread_rng().gen_range(1..=100);
-    println!("This is the secrete number: {secrete_number}");
+    
 
     loop {
         println!("Please enter your guess:");
@@ -22,7 +22,7 @@ fn main() {
         };      
 
         println!("Your guess is {guess}");
-        
+
         match guess.cmp(&secrete_number) {
             Ordering::Less => println!("go higher"),
             Ordering::Greater => println!("go lower"),
