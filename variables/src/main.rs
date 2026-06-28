5418b72b883321b4b0080e59421509ad193fe96a
Mr-Software30 <warzermuhammed5@gmail.com>
2026-06-28 21:20:54 +0300

    Variables directory added

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    //--------- variables and const -------------
+
+    // let x = 5; 
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+
+    //--------- shadowing -------------
+    let z = 8;
+    let z = z + 2;
+
+    {
+        let z = z * 2;
+        println!("The value of z in the inner scope is: {z}");
+    }
+
+    println!("The value of z in the main fn scope is: {z}");
+}
