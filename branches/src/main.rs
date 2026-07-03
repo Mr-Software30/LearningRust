29c703930a420072bc5bb6e30a70d9f1857cd008
Mr-Software30 <warzermuhammed5@gmail.com>
2026-07-03 22:16:46 +0300

    if expressions

---
+++ branches/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // let number = 7;
+
+    // if number < 5 {
+    //     println!("condition was true");
+    // } else {
+    //     println!("condition was false");
+    // }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    /*
+    Rust needs to know definitively at compile time what type the number variable is. 
+    Knowing the type of number 
+    lets the compiler verify the type is valid everywhere we use number.
+    */
+
+}
