9a98c205a98b79dd20859b970493a53e8dade6ca
Mr-Software30 <warzermuhammed5@gmail.com>
2026-07-08 22:42:03 +0300

    multiple mutable references

--- copy_and_drop_trait/src/main.rs
+++ copy_and_drop_trait/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 
     //s.push_str(" my friend");     // s is not mut  
     println!("{s}");
-    
+
     takes_ownership(s);             // s's value moves into the function, and so is no longer valid here
 
     //println!("{s}");                // error
@@ -15,6 +15,27 @@ fn main() {
                                     // so it's okay to use x afterward.
 
     println!("{}" , x + 2);
+
+
+
+    let mut s = String::from("hello");
+
+        {
+            let r1 = &mut s;
+
+            r1.push_str(" world");
+            println!{"r1 is: {r1}"};
+        } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+        {    
+            let r2 = &mut s;
+
+            r2.push_str(", and goodbye");
+            println!{"r2 is: {r2}"};
+        }
+
+    println!{"final string is: {s}"};
+
 
 } // Here, x goes out of scope, then s. However, because s's value was moved,
   // nothing special happens.
