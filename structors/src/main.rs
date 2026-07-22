2efc77ecb740653fb8dabe773cb1d01eeed4bd95
Mr-Software30 <warzermuhammed5@gmail.com>
2026-07-22 21:30:03 +0300

    field init shorthand

--- structors/src/main.rs
+++ structors/src/main.rs
@@ -8,15 +8,22 @@ struct User {
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
-        username: username,
-        email: email,
+        username,
+        email,
         sign_in_count: 1,
     }
 }
 
 fn main() {
-    let user1 = build_user(
+    let mut user1 = build_user(
         String::from("someone@example.com"),
         String::from("someusername123"),
     );
+
+    user1.email = String::from("anyEmail@example.com");
+
+    println!("User 1 email: {}", user1.email);
+    println!("User 1 username: {}", user1.username);
+    println!("User 1 active: {}", user1.active);
+    println!("User 1 sign in count: {}", user1.sign_in_count);
 }
