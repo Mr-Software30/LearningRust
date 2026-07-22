b5ae0f494e4c027910f991f694cb9c202fb7842a
Mr-Software30 <warzermuhammed5@gmail.com>
2026-07-23 01:09:32 +0300

    changes in values

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -49,14 +49,15 @@ fn main() {
     let square1 = Rectangle::square(20);
 
     println!(
-        "The area of the rectangle is is {} square pixels.",
-        rect1.area()
+        "The area of the square is is {} square pixels.\n",
+        square1.area()
     );
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     println!("Can rect1 hold rect4? {}", rect1.can_hold(&rect4));
     println!("Can rect1 hold square1? {}", rect1.can_hold(&square1));
+    println!("\nCan square1 hold square1? {}", square1.can_hold(&square1));
 
     //dbg!(&rect1); //unlike println!, dbg! does take ownership of the value.
 
