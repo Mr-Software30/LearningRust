6b9a990c5f345911d62cf2dc95ec32c72f2dc026
Mr-Software30 <warzermuhammed5@gmail.com>
2026-07-22 22:13:40 +0300

    tuple structs :|

--- structors/src/main.rs
+++ structors/src/main.rs
@@ -5,6 +5,11 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+struct Cordinates(i32, i32, i32);
+
+
+
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
@@ -20,10 +25,31 @@ fn main() {
         String::from("someusername123"),
     );
 
-    user1.email = String::from("anyEmail@example.com");
 
-    println!("User 1 email: {}", user1.email);
-    println!("User 1 username: {}", user1.username);
-    println!("User 1 active: {}", user1.active);
-    println!("User 1 sign in count: {}", user1.sign_in_count);
+    let user2 = User {
+        email: String::from("user2@something.com"),
+        ..user1
+    };
+
+    user1.username = String::from("user1Updated");
+
+    // println!("User 1 email: {}", user1.email);
+    // println!("User 1 username: {}", user1.username);
+    // println!("User 1 active: {}", user1.active);
+    // println!("User 1 sign in count: {}", user1.sign_in_count);
+
+    println!("User 2 email: {}", user2.email);
+    println!("User 2 username: {}", user2.username);
+    println!("User 2 active: {}", user2.active);
+    println!("User 2 sign in count: {}", user2.sign_in_count);
+
+    let white = Color(255, 255, 255);
+    let center = Cordinates(0, 0, 0);
+
+    let Color(r, g, b) = white;
+    let Cordinates(x, y, z) = center;
+
+    println!("\nRGB values for white: {r}, {g}, {b}");
+    println!("Cordinates for center: {x}, {y}, {z}");
+
 }
