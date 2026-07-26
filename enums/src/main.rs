210b0703b26148431175574bda410a9c97286878
Mr-Software30 <warzermuhammed5@gmail.com>
2026-07-26 21:33:21 +0300

    match construct

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,5 @@
+use crate::UsState::Alaska;
+
 #[derive(Debug)]
 enum IpAddr {
         V4(i8, i8, i8, i8),
@@ -8,7 +10,19 @@ enum IpAddr {
 //     Some(T),
 //     None,
 // }
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
 
+#[derive(Debug)]
+enum UsCoin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
 
 fn main() {
     
@@ -19,4 +33,27 @@ fn main() {
     println!("Home: {:?}", home);
     println!("Loopback: {:?}", loopback);
 
+    value_in_cents(UsCoin::Quarter(UsState::Alaska));
+    value_in_cents(UsCoin::Penny);
+    value_in_cents(UsCoin::Dime);
+    let coin1 = UsCoin::Penny;
+
+    println!("value of a random coin is: {}", value_in_cents(UsCoin::Dime));
+    println!("value of coin1: {}", value_in_cents(coin1)); // the matching patter will be displayed first, then the "value of coin1"
+
+}
+
+fn value_in_cents(coin: UsCoin) -> u8 {
+    match coin {
+        UsCoin::Penny => {
+            println!("Lucky penny huh?");
+            1
+        }
+        UsCoin::Nickel => 5,
+        UsCoin::Dime => 10,
+        UsCoin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
 }
